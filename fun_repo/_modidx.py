9a1013b2df56d213f913d2f7ace56621c56fe781
Ideapad-310 <masterdeven@gmail.com>
2023-01-08 13:07:05 -0500

    fixes #1

--- fun_repo/_modidx.py
+++ fun_repo/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://deven367.github.io',
                 'git_url': 'https://github.com/deven367/fun-repo',
                 'lib_path': 'fun_repo'},
-  'syms': {'fun_repo.core': {'fun_repo.core.foo': ('core.html#foo', 'fun_repo/core.py')}}}
+  'syms': {'fun_repo.core': {'fun_repo.core.foo': ('core.html#foo', 'fun_repo/core.py')}, 'fun_repo.lovely': {}}}
